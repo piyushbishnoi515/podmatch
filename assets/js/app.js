2905abc2e041ed6f81fc4809d25066fad5e9982b
piyushbishnoi515 <piyushbishnoi29129@gmail.com>
2024-07-10 12:41:05 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+function openNav() {
+    document.getElementById("navbar").classList.toggle("start-0");
+    document.body.classList.toggle("overflow_hidden");
+    document.querySelector(".menu_1").classList.toggle("cross")
+}
